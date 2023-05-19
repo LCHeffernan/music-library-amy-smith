b359612e20b79fcda4eb4f667cd02acc29b94a4d
AmyHMSmith <amyhmsmith@gmail.com>
2023-05-19 15:34:42 +0100

    update artists with PUT

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,3 +1,4 @@
+const app = require("../app");
 const db = require("../db");
 
 const createArtist = async (req, res) => {
@@ -7,7 +8,7 @@ const createArtist = async (req, res) => {
     const {
       rows: [artist],
     } = await db.query(
-      `INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *`,
+      "INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *",
       [name, genre]
     );
     res.status(201).json(artist);
@@ -16,4 +17,53 @@ const createArtist = async (req, res) => {
   }
 };
 
-module.exports = { createArtist };
+const getArtistById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      rows: [artist],
+    } = await db.query("SELECT * FROM Artists WHERE id = $1", [id]);
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+const getArtists = async (_, res) => {
+  try {
+    const { rows } = await db.query("SELECT * FROM Artists");
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+const putArtist = async (req, res) => {
+  const { id } = req.params;
+  const { name, genre } = req.body;
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(
+      "UPDATE Artists SET name = $1, genre = $2 WHERE id = $3 RETURNING *",
+      [name, genre, id]
+    );
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = { createArtist, getArtists, getArtistById, putArtist };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const { createArtist } = require("../controllers/artist");
+const {
+  createArtist,
+  getArtists,
+  getArtistById,
+  putArtist,
+} = require("../controllers/artist");
 
 const router = express.Router();
 
 router.route("/").post(createArtist);
+
+router.route("/").get(getArtists);
+
+router.route("/:id").get(getArtistById);
+
+router.route("/:id").put(putArtist);
 
 module.exports = router;
